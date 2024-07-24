dbaef3f6beb6a53c6aa5e821d09e888946cc9a8e
Nguyen1976 <Nguyen2202794@gmail.com>
2024-07-24 15:10:38 +0700

    plan 2

--- Day 22/main.js
+++ Day 22/main.js
@@ -5,13 +5,13 @@
 //     btn.addEventListener('click', (e) => {
 //         let type = e.target.getAttribute('type-food');
     
-//         btnList.forEach(btn => {
-//             if(btn.getAttribute('type-food') === type) {
-//                 btn.classList.add('active');
-//             } else {
-//                 btn.classList.remove('active');
-//             }
-//         })
+        // btnList.forEach(btn => {
+        //     if(btn.getAttribute('type-food') === type) {
+        //         btn.classList.add('active');
+        //     } else {
+        //         btn.classList.remove('active');
+        //     }
+        // })
 
 //         imgList.forEach(img => {
 //             let foodType = img.getAttribute('type-food');
@@ -27,3 +27,42 @@
 //         })
 //     })
 // })
+
+var btnList = document.querySelectorAll('button')
+var imgList = document.querySelectorAll('img')
+
+var arr = [];
+imgList.forEach(item => {
+    arr.push({
+        src: item.getAttribute('src'),
+        foodType: item.parentElement.getAttribute('type-food')
+    })
+})
+
+btnList.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        let type = e.target.getAttribute('type-food');
+        btnList.forEach(btn => {
+            if(btn.getAttribute('type-food') === type) {
+                btn.classList.add('active');
+            } else {
+                btn.classList.remove('active');
+            }
+        })
+        render(type);
+    })
+})
+
+function render(type) {
+    var html = arr.map(item => {
+        if(item.foodType === type || type === 'all') {
+            return `
+                <div class="food-item" type-food="${item.foodType}">
+                    <img src="${item.src}" alt="">
+                </div>
+            `
+        }
+    }).join(' ');
+    document.querySelector('.food-list').innerHTML = html;
+}
+console.log(arr)
