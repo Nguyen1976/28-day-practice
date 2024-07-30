fdc7de9fcd042ef8d9fbbc641578b5bec0348b92
Nguyen1976 <Nguyen2202794@gmail.com>
2024-07-30 20:05:30 +0700

    Fix main.js

--- Day 28/main.js
+++ Day 28/main.js
@@ -3,14 +3,12 @@ const title = document.querySelector('.content__title')
 const text = document.querySelector('.content__text')
 const button = document.querySelector('button')
 const content = document.querySelector('.content')
+const container = document.querySelector('.container')
 
 setTimeout(() => {
     content.style.color = '#000';
     img.querySelector('img').src = './img.avif'
     button.style.color = '#fff';
 
-    img.classList.remove('loading')
-    title.classList.remove('loading')
-    text.classList.remove('loading')
-    button.classList.remove('loading')
-}, 4000)
+    container.classList.remove('loading');
+}, 3000)
